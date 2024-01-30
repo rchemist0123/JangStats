f22390cf6e6a209a40d291a0cf1f53981df61c03
rchemist0123 <aidenjang0123@gmail.com>
2024-01-30 12:04:23 +0900

    add functions for merging uni & multi tbls

---
+++ R/coxTableCombine.R
@@ -0,0 +1,44 @@
+#' Combine univariable & multivariable Cox regression table
+#'
+#' Perform univariable Cox regressions for numerous variables and create a table
+#' @param data A data for analysis
+#' @param uni_tbl A data of univariable Cox regression table
+#' @param mult_tbl A data of multivariable Cox regression table
+#' @return A table of merged table of Cox regressions
+#' @importFrom gt gt tab_style cell_fill cells_body cols_label md ends_with tab_spanner
+#' @export
+
+coxTableCombine = function(data, uni_tbl, mult_tbl){
+  df = merge(uni_tbl, mult_tbl, by="variable")
+  tbl = df |>
+    gt() |>
+    cols_label(
+      variable = md("**Variable**"),
+      HR_ci.x = md("**HR (95% CI)**"),
+      p.x = md("**P value**"),
+      HR_ci.y = md("**HR (95% CI)**"),
+      p.y = md("**P value**")
+    ) |>
+    tab_style(
+      style = list(
+        cell_fill(color="lightyellow")
+      ),
+      locations = cells_body(
+        rows = df$p.x < 0.05,
+      )
+    ) |>
+    tab_style(
+      style = list(
+        cell_fill(color="lightyellow")
+      ),
+      locations = cells_body(
+        rows = df$p.y < 0.05,
+      )
+    ) |>
+    tab_spanner(label=md("**Univariable**"),
+                columns = ends_with(".x")) |>
+    tab_spanner(label=md("**Multivariable**"),
+                columns = ends_with(".y")
+    )
+  return(tbl)
+}
---
+++ R/lrTableCombine.R
@@ -0,0 +1,44 @@
+#' Combine univariable & multivariable Logistic regression table
+#'
+#' Perform univariable logistic regressions for numerous variables and create a table
+#' @param data A data for analysis
+#' @param uni_tbl A data of univariable logistic regression table
+#' @param mult_tbl A data of multivariable logistic regression table
+#' @return A table of merged table of logistic regressions
+#' @importFrom gt gt tab_style cell_fill cells_body cols_label md ends_with tab_spanner
+#' @export
+
+lrTableCombine = function(data, uni_tbl, mult_tbl){
+  df = merge(uni_tbl, mult_tbl, by="variable")
+  tbl = df |>
+    gt() |>
+    cols_label(
+      variable = md("**Variable**"),
+      OR_ci.x = md("**OR (95% CI)**"),
+      p.x = md("**P value**"),
+      OR_ci.y = md("**OR (95% CI)**"),
+      p.y = md("**P value**")
+    ) |>
+    tab_style(
+      style = list(
+        cell_fill(color="lightyellow")
+      ),
+      locations = cells_body(
+        rows = df$p.x < 0.05,
+      )
+    ) |>
+    tab_style(
+      style = list(
+        cell_fill(color="lightyellow")
+      ),
+      locations = cells_body(
+        rows = df$p.y < 0.05,
+      )
+    ) |>
+    tab_spanner(label=md("**Univariable**"),
+                columns = ends_with(".x")) |>
+    tab_spanner(label=md("**Multivariable**"),
+                columns = ends_with(".y")
+    )
+  return(tbl)
+}
