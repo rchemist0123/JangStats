b832ec9cec579d3d14c4306cff3661d48fd42309
rchemist0123 <aidenjang0123@gmail.com>
2024-01-31 16:46:53 +0900

    fix error;

--- R/varMissTable.R
+++ R/varMissTable.R
@@ -5,17 +5,20 @@
 #' @param include Variables to check the number of missing values. if NA, all variables in data are included.
 #' @importFrom data.table as.data.table .SD melt dcast setorderv
 #' @importFrom gt gt tab_header
+#' @examples
+#' # example code
+#' varMisstable(airquality)
 #' @export
 varMissTable = function(data, include=NULL){
   if(length(include) > 0){
     x_n = as.data.table(data)[,lapply(.SD, \(x) sum(is.na(x))),.SDcols = include]
     x_rate = as.data.table(data)[,lapply(.SD, \(x) sum(is.na(x))/nrow(data)*100),.SDcols=include]
   } else {
-    x_n = as.data.table(data)[,lapply(.SD, \(x) sum(is.na(x)))]
-    x_rate = as.data.table(data)[,lapply(.SD, \(x) round(sum(is.na(x))/nrow(data)*100,2))]
+    x_n = as.data.table(data)[,lapply(.SD, \(x) sum(is.na(x))), .SDcols = names(data)]
+    x_rate = as.data.table(data)[,lapply(.SD, \(x) round(sum(is.na(x))/nrow(data)*100,2)), .SDcols = names(data)]
   }
   df = rbind(x_n, x_rate)
-  df[['name']] = rep(c('N','rate(%)'), length(include))
+  df[['name']] = c('N','rate(%)')
   x_melt = melt(df, id.vars='name')
   x_dcast = dcast(x_melt, variable ~ name, value.var = 'value')
   setorderv(x_dcast, "N", -1)
