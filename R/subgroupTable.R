70d43a13742a1ce64f4f69e5de72c665df6addf5
rchemist0123 <aidenjang0123@gmail.com>
2024-02-08 09:40:41 +0900

    fix

--- R/subgroupTable.R
+++ R/subgroupTable.R
@@ -3,15 +3,20 @@
 #' Perform a subgroup analysis and create a table
 #' @param data A data for analysis
 #' @param fit A logistic regression or Cox regression model
-#' @param treatment A variable that
-#' @param subgroups Categorical Variable(s) for the subgroup analysis
+#' @param treatment A variable that could divide case & control group.
+#' @param subgroups Categorical Variable(s) for the subgroup analysis.
 #' @param digits Digits of result values. Default as 2.
 #' @return A table of Subgroup analysis with subgroups, level, case, control,  estimates, ci, and p interaction
 #' @importFrom Publish subgroupAnalysis
 #' @importFrom dplyr group_by
 #' @importFrom gt gt tab_style cols_label cols_label md fmt_number cols_align cells_row_groups cell_text
 #' @export
-subAnalTable = function(data, fit, treatment, subgroups, digits=2){
+subgroupTable = function(data, fit, treatment, subgroups, digits=2){
+  classes = sapply(data[,c(treatment, subgroups), with=F], class)
+  if(!classes[treatment] == 'factor') data[[treatment]] = as.factor(data[[treatment]])
+  for(s in subgroups){
+     if(!classes[s] == 'factor') data[[s]] = as.factor(data[[s]])
+  }
   stopifnot("No subgroups are provided." = length(subgroups) == 0)
   tbl = subgroupAnalysis(fit,
                    data = data,
