d69c247bd779976abbf045d35d08e9aa2b92179e
rchemist0123 <aidenjang94@gmail.com>
2024-10-08 08:34:29 +0900

    change

--- R/nprop.R
+++ R/nprop.R
@@ -4,20 +4,24 @@
 #' @param x A vector to check frequency and proportion
 #' @param digits A digit for proportions
 #' @param missing Whether considering NA values. Default is FALSE.
+#' @param paste Whehter to paste freq and prop. Default is FALSE.
 #' @export
 #' @examples
 #' library(data.table)
 #' as.data.table(mtcars)[,nProp(cyl)]
 #'
-nProp = function(x, digits = 1, missing=FALSE){
+nProp = function(x, digits = 1, missing=FALSE, paste = FALSE){
   if(!missing) n = table(x)
   else n = table(x, useNA = "ifany")
   p = round(proportions(n)*100, digits) |> as.vector();
   name = deparse(substitute(x))
   l = list()
   l[[name]] = names(n);
-  l[['N']] = as.vector(n);
-  l[['P']] = p
-  # print(l)
+  if(paste){
+    l[['N(%)']] = sprintf("%s (%s%%)", as.vector(n), format(p, nsmall=digits))
+  } else {
+    l[['N']] = as.vector(n);
+    l[['P']] = p
+  }
   return(l)
 }
